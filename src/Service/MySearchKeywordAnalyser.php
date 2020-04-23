65f218cf1c3769f8deb3b12125c1b97e1d6ed162
Moritz Naczenski <m.naczenski@shopware.com>
2020-04-23 08:40:40 +0200

    add plugin config, support for metadescription and categorynames

--- src/Service/MySearchKeywordAnalyser.php
+++ src/Service/MySearchKeywordAnalyser.php
@@ -6,6 +6,9 @@ use Shopware\Core\Content\Product\SearchKeyword\AnalyzedKeywordCollection;
 use Shopware\Core\Content\Product\SearchKeyword\AnalyzedKeyword;
 use Shopware\Core\Content\Product\SearchKeyword\ProductSearchKeywordAnalyzerInterface;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Term\TokenizerInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Shopware\Core\Framework\Context;
 
 class MySearchKeywordAnalyser implements ProductSearchKeywordAnalyzerInterface
@@ -21,24 +24,69 @@ class MySearchKeywordAnalyser implements ProductSearchKeywordAnalyzerInterface
      */
     private $tokenizer;
 
+    /**
+     * @var EntityRepositoryInterface
+     */
+    private $categoryRepository;
 
-    public function __construct(ProductSearchKeywordAnalyzerInterface $coreAnalyzer, TokenizerInterface $tokenizer)
+    /**
+     * @var SystemConfigService
+     */
+    private $systemConfigService;
+
+    public function __construct(ProductSearchKeywordAnalyzerInterface $coreAnalyzer, TokenizerInterface $tokenizer, SystemConfigService $systemConfigService, EntityRepositoryInterface $categoryRepository)
     {
         $this->coreAnalyzer = $coreAnalyzer;
         $this->tokenizer = $tokenizer;
+        $this->systemConfigService = $systemConfigService;
+        $this->categoryRepository = $categoryRepository;
     }
     public function analyze(ProductEntity $product, Context $context): AnalyzedKeywordCollection
     {
 
         $keywords = $this->coreAnalyzer->analyze($product, $context);
 
-        $description = $product->getTranslation('description');
+        if ($this->systemConfigService->get('MNExtendSearch.config.description') == true) {
+            $description = $product->getTranslation('description');
 
+            $ranking = $this->systemConfigService->get('MNExtendSearch.config.rankingdescription');
 
-        if ($description) {
-            $tokens = $this->tokenizer->tokenize((string) $description);
-            foreach ($tokens as $token) {
-                $keywords->add(new AnalyzedKeyword((string) $token, 800));
+            if ($description) {
+                $tokens = $this->tokenizer->tokenize((string) $description);
+                foreach ($tokens as $token) {
+                    $keywords->add(new AnalyzedKeyword((string) $token, $ranking));
+                }
+            }
+        }
+
+        if ($this->systemConfigService->get('MNExtendSearch.config.metadescription') == true) {
+            $metadescription = $product->getTranslation('metaDescription');
+
+            $ranking = $this->systemConfigService->get('MNExtendSearch.config.rankingmetadescription');
+
+            if ($metadescription) {
+                $tokens = $this->tokenizer->tokenize((string) $metadescription);
+                foreach ($tokens as $token) {
+                    $keywords->add(new AnalyzedKeyword((string) $token, $ranking));
+                }
+            }
+        }
+
+        if ($this->systemConfigService->get('MNExtendSearch.config.categories') == true) {
+
+            $categories = $product->getCategoryTree();
+            $categories = $this->categoryRepository->search(
+                new Criteria($categories),
+                \Shopware\Core\Framework\Context::createDefaultContext()
+            );
+
+            $ranking = $this->systemConfigService->get('MNExtendSearch.config.rankingcategories');
+
+            if ($categories) {
+                foreach ($categories as $category) {
+                    $categoryName = $category->getTranslation('name');
+                    $keywords->add(new AnalyzedKeyword((string) $categoryName, $ranking));
+                }
             }
         }
 
